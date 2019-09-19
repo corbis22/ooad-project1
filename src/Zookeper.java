1eb00cd2bc8170437f77d0d5723fd06b356c04fd
Daniel Taylor <Isomorph@protonmail.com>
2019-09-19 15:58:03 -0600

    Fixed some issues related to imports

--- src/Zookeper.java
+++ src/Zookeper.java
@@ -1,44 +1,44 @@
-import Animal;
+import java.util.List;
 
 public class Zookeper
 {
-    public void wakeAnimals(List<Animal> zoo) {
-        System.out.println("Waking up the animals...");
-        for (Animal animal : zoo) {
-            animal.wakeUp();
-        }
-        System.out.println("All the animals are awake!");
-    }
+	public void wakeAnimals(List<Animal> zoo) {
+		System.out.println("Waking up the animals...");
+		for (Animal animal : zoo) {
+			animal.wakeUp();
+		}
+		System.out.println("All the animals are awake!");
+	}
 
-    public void rollCallAnimals(List<Animal> zoo) {
-        System.out.println("Taking roll call...");
-        for (Animal animal : zoo) {
-            animal.makeNoise():
-        }
-        System.out.println("That's everybody!");
-    }
+	public void rollCallAnimals(List<Animal> zoo) {
+		System.out.println("Taking roll call...");
+		for (Animal animal : zoo) {
+			animal.makeNoise();
+		}
+		System.out.println("That's everybody!");
+	}
 
-    public void feedAnimals(List<Animal> zoo) {
-        System.out.println("Feeding the animals...");
-        for (Animal animal : zoo) {
-            //TODO: add foods/probs for the eat behavior
-            animal.eat();
-        }
-        System.out.println("The food is all gone!");
-    }
+	public void feedAnimals(List<Animal> zoo) {
+		System.out.println("Feeding the animals...");
+		for (Animal animal : zoo) {
+			//TODO: add foods/probs for the eat behavior
+			animal.eat();
+		}
+		System.out.println("The food is all gone!");
+	}
 
-    public void exerciseAnimals(List<Animal> zoo) {
-        System.out.println("Time for a workout...");
-        for (Animal animal : zoo) {
-            animal.roam();
-        }
-        System.out.println("All in a day's work!");
-    }
+	public void exerciseAnimals(List<Animal> zoo) {
+		System.out.println("Time for a workout...");
+		for (Animal animal : zoo) {
+			animal.roam();
+		}
+		System.out.println("All in a day's work!");
+	}
 
-    public void shutDownZoo(List<Animal> zoo) {
-        System.out.println("Lights out...");
-        for (Animal animal : zoo) {
-            animal.goToSleep();
-        }
-    }
+	public void shutDownZoo(List<Animal> zoo) {
+		System.out.println("Lights out...");
+		for (Animal animal : zoo) {
+			animal.goToSleep();
+		}
+	}
 }
