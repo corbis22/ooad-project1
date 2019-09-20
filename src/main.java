034f72b415df1a108416b43285742e246c650c13
Corey Stephens <35537767+corbis22@users.noreply.github.com>
2019-09-19 18:51:03 -0600

    Added basic method implementations for Zookeeper May need to modify/expand on these

--- src/main.java
+++ src/main.java
@@ -1,6 +1,32 @@
+import java.util.List;
+
 public class main
 {
 	public static void main(String[] args)
 	{
+	    List<Animal> zoo = new ArrayList();
+	    zoo.add(new Dog("Doug"));
+	    zoo.add(new Dog("Diane"));
+		zoo.add(new Elephant("Earl"));
+		zoo.add(new Elephant("Elanor"));
+		zoo.add(new Hippo("Harry"));
+		zoo.add(new Hippo("Helen"));
+		zoo.add(new HouseCat("Herman"));
+		zoo.add(new HouseCat("Heather"));
+		zoo.add(new Lion("Luigi"));
+		zoo.add(new Lion("Laura"));
+		zoo.add(new Rhino("Ryan"));
+		zoo.add(new Rhino("Rachel"));
+		zoo.add(new Tiger("Tim"));
+		zoo.add(new Tiger("Tina"));
+		zoo.add(new Wolf("Warren"));
+		zoo.add(new Wolf("Wendy"));
+
+		Zookeeper keeper = new Zookeeper();
+		keeper.wakeAnimals(zoo);
+		keeper.rollCallAnimals(zoo);
+		keeper.feedAnimals(zoo);
+		keeper.exerciseAnimals(zoo);
+		keeper.shutDownZoo(zoo);
 	}
 }
